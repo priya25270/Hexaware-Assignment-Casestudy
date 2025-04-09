884fbd858f38703d5a7935169a32a924be10672a
priya25270 <108729878+priya25270@users.noreply.github.com>
2025-04-10 00:19:16 +0530

    Add files via upload

---
+++ Assignment/SIS_ADO/Data/DBUtility.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AdoConnectedDemo.Data
+{
+    internal class DBUtility
+    {
+        
+        const string connectionString = @"Data Source=LAPTOP-V71FT21G\SQLEXPRESS; Initial Catalog=hexa; Integrated Security=True; MultipleActiveResultSets=true;";
+
+        public static SqlConnection GetConnection()
+        {
+            SqlConnection connectionObject = new SqlConnection(connectionString);
+
+            try
+            {
+                connectionObject.Open();
+                return connectionObject;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error opening the connection: {e.Message}");
+                return null;
+            }
+        }
+
+        public static void CloseDbConnection(SqlConnection connectionObject)
+        {
+            if (connectionObject != null)
+            {
+                try
+                {
+                    if (connectionObject.State == ConnectionState.Open)
+                    {
+                        connectionObject.Close();
+                        connectionObject.Dispose();
+                        Console.WriteLine("Connection closed successfully.");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Error closing connection: {e.Message}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Connection is already null.");
+            }
+        }
+    }
+}
---
+++ Assignment/SIS_ADO/Model/Payment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIS_ADO.Model
+{
+    public class Payment
+    {
+        public int PaymentId { get; set; }
+        public int StudentId { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime PaymentDate { get; set; }
+    }
+}
